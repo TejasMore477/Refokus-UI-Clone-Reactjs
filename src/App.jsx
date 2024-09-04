7dce6410b53ec9e11ab6afb233561a759b1f770e
Tejas More <tejasmore660@gmail.com>
2024-09-04 20:23:36 +0530

    installed smooth scroll through locomotive

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import Stripes from './components/Stripes'
 import Products from './components/Products';
 import Cards from './components/Cards';
 import Footer from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
+
+
 
 
 function App (){
+  const locomotiveScroll = new LocomotiveScroll();
   return(
     <div className='w-full text-white bg-zinc-900 font-["Satoshi_Variable"]'>
       <Navbar/>
