4aa8e1d34bc99d222786dae64fed0558f5bef97d
Tejas More <tejasmore660@gmail.com>
2024-09-05 13:18:59 +0530

    removig basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Make sure to install react-router-dom
+import { BrowserRouter } from 'react-router-dom'; 
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/Refokus-UI-Clone-Reactjs/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
