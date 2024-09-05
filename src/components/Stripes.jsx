46b4f0423db5005bff7b683ff63fdb3568d9a0f7
Tejas More <tejasmore660@gmail.com>
2024-09-05 13:31:25 +0530

    importing images at marquee

--- src/components/Stripes.jsx
+++ src/components/Stripes.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Stripe from "./Stripe";
 
+import image1 from '../assets/marquee/item1.svg'
+import image2 from '../assets/marquee/item2.svg'
+import image3 from '../assets/marquee/item3.svg'
+import image4 from '../assets/marquee/item6.svg'
+import image5 from '../assets/marquee/item7.svg'
+import image6 from '../assets/marquee/item8.svg'
+
 function Stripes() {
   const data = [
-    { url: "src/assets/marquee/item1.svg", numb: 34 },
-    { url: "src/assets/marquee/item2.svg", numb: 42 },
-    { url: "src/assets/marquee/item3.svg", numb: 12 },
-    { url: "src/assets/marquee/item6.svg", numb: 16 },
-    { url: "src/assets/marquee/item7.svg", numb: 31 },
-    { url: "src/assets/marquee/item8.svg", numb: 43 },
+    { url: image1, numb: 34 },
+    { url: image2, numb: 42 },
+    { url: image3, numb: 12 },
+    { url: image4, numb: 16 },
+    { url: image5, numb: 31 },
+    { url: image6, numb: 43 },
   ];
   return (
     <div className="flex items-center justify-between overflow-x-auto flex-nowrap mt-28">
