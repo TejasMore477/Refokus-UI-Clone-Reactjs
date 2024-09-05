8475c0b6892d499e385f5399f5ad29bfc340bfcb
Tejas More <tejasmore660@gmail.com>
2024-09-05 13:35:52 +0530

    importing videos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,14 +2,20 @@ import React, { useState } from 'react'
 import Product from './Product'
 import { motion } from 'framer-motion'
 
+import video1 from '../assets/videos/v1.webm'
+import video2 from '../assets/videos/v2.webm'
+import video3 from '../assets/videos/v3.webm'
+import video4 from '../assets/videos/v4.webm'
+import video5 from '../assets/videos/v5.webm'
+
 function Products() {
 
     const data = [
-        {title:"Webflow",bg:"hover:bg-black",video:"src/assets/videos/v1.webm",para:"An interactive learning game that can educate and entertain you on the basics of web layouts in Webflow.",live:true,case:false},
-        {title:"Cula",bg:"hover:bg-[#4A576B]",video:"src/assets/videos/v2.webm",para:"We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",live:true,case:false},
-        {title:"Singularity",bg:"hover:bg-[#000000]",video:"src/assets/videos/v3.webm",para:"A new site for Singularity that supports their growth and showcases their true identity as industry innovators.",live:true,case:true},
-        {title:"Brightwave",bg:"hover:bg-[#303040]",video:"src/assets/videos/v4.webm",para:"Brightwave, a category disruptor came to a us to build their brand from scratch and make a statement on its launch.",live:true,case:false},
-        {title:"ARQITEL",bg:"hover:bg-[#050987]",video:"src/assets/videos/v5.webm",para:"With a continuous 3D animation, we showcase Arqitel's approach and show how migration data translates into real estate.",live:true,case:true},
+        {title:"Webflow",bg:"hover:bg-black",video:video1,para:"An interactive learning game that can educate and entertain you on the basics of web layouts in Webflow.",live:true,case:false},
+        {title:"Cula",bg:"hover:bg-[#4A576B]",video:video2,para:"We immersed ourselves in a 3D world we created to explain how Cula's platform collects data from carbon removal processes and converts them into carbon credit certificates.",live:true,case:false},
+        {title:"Singularity",bg:"hover:bg-[#000000]",video:video3,para:"A new site for Singularity that supports their growth and showcases their true identity as industry innovators.",live:true,case:true},
+        {title:"Brightwave",bg:"hover:bg-[#303040]",video:video4,para:"Brightwave, a category disruptor came to a us to build their brand from scratch and make a statement on its launch.",live:true,case:false},
+        {title:"ARQITEL",bg:"hover:bg-[#050987]",video:video5,para:"With a continuous 3D animation, we showcase Arqitel's approach and show how migration data translates into real estate.",live:true,case:true},
     ];
 
     const [pose, setPose] = useState(0);
